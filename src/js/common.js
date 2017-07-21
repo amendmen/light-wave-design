e0479f4b7dd987d8f32f91314c7d071fba18bf13
Alex <amendment000@gmail.com>
2017-07-21 13:38:37 +0300

    nav-menu <768

--- src/js/common.js
+++ src/js/common.js
@@ -1,36 +1,69 @@
 $(document).ready(function() {
 
+	
+		if ( $(window).width() < 768) {
+			$('#modal-menu').addClass('mfp-hide white-popup-block');
+		}
+		else { 
+			$('#modal-menu').removeClass('mfp-hide white-popup-block');
+		}
+
+	
+	$(window).resize(function() {
+		if ($(window).width() < 768) {
+			$('#modal-menu').addClass('mfp-hide white-popup-block');
+		}
+		else {
+			$('#modal-menu').removeClass('mfp-hide white-popup-block');
+		}
+	})
+
+ 
+
+	$(function () {
+	$('.popup-modal').magnificPopup({
+		type: 'inline',
+		preloader: false,
+		closeOnContentClick: true,
+		modal: true
+	});
+	$(document).mouseup( '#modal-menu .go_to', function (e) {
+		e.preventDefault();
+		$.magnificPopup.close();
+	});
+});
+
 	$(function(){
 		$('#Container').mixItUp();
 	});
 
 	$('.go_to').click( function(){ 
-  		var scroll_el = $(this).attr('href'); 
-  		if ($(scroll_el).length != 0) { 
-  			$('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 500); 
-  		}
-  		return false; 
-  	}); 
+		var scroll_el = $(this).attr('href'); 
+		if ($(scroll_el).length != 0) { 
+			$('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 500); 
+		}
+		return false; 
+	}); 
 
 	$(function() {	
 		$('.vegas-container').vegas({
-		delay: 7000,
-		timer: false,
-        slides: [
-            { src: '/img/1.jpg' },
-            { src: '/img/2.jpg' },
-            { src: '/img/3.jpg' },
-            { src: '/img/4.jpg' }
-        ],
-         transition: 'fade2',
-         animation: 'random',
-         overlay: '/img/overlays/02.png'
-         
+			delay: 7000,
+			timer: false,
+			slides: [
+			{ src: '/img/1.jpg' },
+			{ src: '/img/2.jpg' },
+			{ src: '/img/3.jpg' },
+			{ src: '/img/4.jpg' }
+			],
+			transition: 'fade2',
+			animation: 'random',
+			overlay: '/img/overlays/02.png'
 
-    });
-})
 
-$('.image-popup-fit-width').magnificPopup({
+		});
+	})
+
+	$('.image-popup-fit-width').magnificPopup({
 		type: 'image',
 		closeOnContentClick: true,
 		image: {
@@ -38,11 +71,11 @@ $('.image-popup-fit-width').magnificPopup({
 		}
 	});
 
-$('body').scrollspy({ target: '.navbar' });
+	$('body').scrollspy({ target: '.navbar' });
 
 
-$('.carousel').carousel({
-  interval: 2000
-});
+	$('.carousel').carousel({
+		interval: 2000
+	});
 
 })
